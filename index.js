6838bad5c01f13339c3f0fbcdecfda04b7391a89
tim.rogers <tim.rogers@justeattakeaway.com>
2025-03-31 20:32:19 +0100

    Test

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
-import functions from '@google-cloud/functions-framework'
-import { Firestore } from '@google-cloud/firestore'
-const firestore = new Firestore()
+const functions = require('@google-cloud/functions-framework');
 
-functions.http('getUsers', async (req, res) => {
+// Register an HTTP function with the Functions Framework that will be executed
+// when you make an HTTP request to the deployed function's endpoint.
+functions.http('getUsers', (req, res) => {
+  res.send('Hello World!');
+});
 
-  const cookies = firestore.doc('mm_cookies/1')
-  const cookie = await cookies.get()
-  var cookieValue = cookies.exists ? cookie.data().value : null
 
-  if (cookieValue) console.log("Found cookie value", cookieValue)
+// import functions from '@google-cloud/functions-framework'
+// import { Firestore } from '@google-cloud/firestore'
+// const firestore = new Firestore()
 
-  const loginResponse = await fetch("https://membermojo.co.uk/vcgh/signin_password", {
-    "body": `email=${encodeURI('timrog@googlemail.com')}&password=${encodeURI('@c7g.A<y.$adDc)p[DZg')}`,
-    "method": "POST"
-  })
+// functions.http('getUsers', async (req, res) => {
 
-  const newCookie = loginResponse.headers.entries
-    .filter(([k]) => k.toLowerCase == 'set-cookie')
-    .map(([k, v]) => v)
-    .join('; ')
+//   const cookies = firestore.doc('mm_cookies/1')
+//   const cookie = await cookies.get()
+//   var cookieValue = cookies.exists ? cookie.data().value : null
 
-  console.log("new cookie", newCookie)
+//   if (cookieValue) console.log("Found cookie value", cookieValue)
 
-  const loginText = await loginResponse.text()
-  const csrf_token = loginText.match(/"csrf_token":"([^"]+)/)[1]
+//   const loginResponse = await fetch("https://membermojo.co.uk/vcgh/signin_password", {
+//     "body": `email=${encodeURI('timrog@googlemail.com')}&password=${encodeURI('@c7g.A<y.$adDc)p[DZg')}`,
+//     "method": "POST"
+//   })
 
-  console.log("csrf token", csrf_token)
+//   const newCookie = loginResponse.headers.entries
+//     .filter(([k]) => k.toLowerCase == 'set-cookie')
+//     .map(([k, v]) => v)
+//     .join('; ')
 
-  var upstream = await fetch("https://membermojo.co.uk/vcgh/membership/download_members?", {
-    "headers": {
-      "cookie": newCookie
-    },
-    "body": null,
-    "method": "GET"
-  });
+//   console.log("new cookie", newCookie)
 
-  res.send(await upstream.text())
-})
+//   const loginText = await loginResponse.text()
+//   const csrf_token = loginText.match(/"csrf_token":"([^"]+)/)[1]
+
+//   console.log("csrf token", csrf_token)
+
+//   var upstream = await fetch("https://membermojo.co.uk/vcgh/membership/download_members?", {
+//     "headers": {
+//       "cookie": newCookie
+//     },
+//     "body": null,
+//     "method": "GET"
+//   });
+
+//   res.send(await upstream.text())
+// })
