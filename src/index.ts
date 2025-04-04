0c62fb7a5a47c9bd32ead84b25ff5128858e07fc
tim.rogers <tim.rogers@justeattakeaway.com>
2025-04-04 20:57:14 +0100

    Working retriever

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { HttpFunction } from '@google-cloud/functions-framework';
 import { Firestore } from '@google-cloud/firestore';
-import FormData from 'form-data';
 import makeFetchCookie from 'fetch-cookie'
 const firestore = new Firestore()
-const fetchCookie = makeFetchCookie(fetch)
+
+const isLocal = process.env.LOCAL
 
 const mm_email = process.env.SECRET_MM_USERNAME
 if (!mm_email) throw new Error("SECRET_MM_USERNAME not set")
@@ -11,46 +11,90 @@ if (!mm_email) throw new Error("SECRET_MM_USERNAME not set")
 const mm_password = process.env.SECRET_MM_PASSWORD
 if (!mm_password) throw new Error("SECRET_MM_PASSWORD not set")
 
-export const getUsers: HttpFunction = async (req, res) => {
+let localData: { [key: string]: string } = { value: '' };
 
-  console.log("Request received", req.path, req.body)
-
-  const cookies = firestore.doc('mm_cookies/1')
-  const cookieDoc = await cookies.get()
-  var cookie = cookieDoc.get('value')
-
-  if (cookie) console.log("Found cookie value", cookie)
-  const formData = new FormData();
-  formData.append("email", mm_email)
-  formData.append("password", mm_password)
-
-  const loginResponse = await fetchCookie("https://membermojo.co.uk/vcgh/signin_password", {
-    body: formData,
-    method: "POST",
-    headers: {
-      cookie
+function getCookieDoc() {
+  if (isLocal) {
+    return {
+      async get() { return { get: (key: string) => localData[key] } },
+      async set(value: any) { localData = { ...localData, ...value }; console.log("Cookie Jar", localData) }
     }
-  })
+  }
 
-  console.log("login response", loginResponse.status, loginResponse.text())
+  return firestore.doc('mm_cookies/1')
+}
 
-  const newCookie = loginResponse.headers.getSetCookie()
-    .map(x => x.split(';')[0])
-    .join('; ')
 
-  console.log("new cookie", newCookie)
-  if (newCookie.length) await cookies.set({ value: newCookie });
+export const getUsers: HttpFunction = async (req, res) => {
+  console.log("Request received", req.path, req.body)
+  const fetchCookie = makeFetchCookie(fetch)
+  const cookieDoc = getCookieDoc()
+  const cookieDocSnapshot = await cookieDoc.get()
+  const cookieValue = cookieDocSnapshot.get('value')
+  if (cookieValue) {
+    await fetchCookie.cookieJar.setCookie(cookieValue, "https://membermojo.co.uk", { ignoreError: false })
+  }
 
-  //const loginText = await loginResponse.text()
-  //const csrf_token = loginText.match(/"csrf_token":"([^"]+)/)[1]
-  //console.log("csrf token", csrf_token)
+  async function extractToken(text: string) {
+    const match = text.match(/"csrf_token":"([^"]+)/)
+    if (!match?.length) return null
+    const csrf = match[1]
+    await cookieDoc.set({ csrf })
+    return csrf
+  }
 
-  var upstream = await fetchCookie("https://membermojo.co.uk/vcgh/membership/download_members?", {
-    "headers": {
-      "cookie": newCookie
-    },
-    "method": "GET"
-  });
+  async function validateCsrfToken(url: string, csrf: string) {
+    console.log("CSRF", csrf)
+    const response = await fetchCookie(url, {
+      method: "POST",
+      body: `csrf_token=${encodeURIComponent(csrf)}`,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    })
+    console.log("validate response", response.status, response.url)
+    return response
+  }
 
-  res.send(await upstream.text());
+  let upstream: Response
+  let upstreamBody: string
+  const validationLink = req.query['validate']
+  if (validationLink) {
+    const doc = await cookieDoc.get()
+    const csrf = doc.get('csrf')
+    upstream = await validateCsrfToken(validationLink.toString(), csrf)
+    upstreamBody = await upstream.text()
+  }
+  else {
+    upstream = await fetchCookie("https://membermojo.co.uk/vcgh/membership/download_members")
+    upstreamBody = await upstream.text()
+    console.log("download response", upstream.status, upstream.url)
+    var csrf = await extractToken(upstreamBody)
+    if (csrf) {
+      upstream = await fetchCookie("https://membermojo.co.uk/vcgh/signin_password", {
+        body: `email=${encodeURIComponent(mm_email)}&password=${encodeURIComponent(mm_password)}&csrf_token=${encodeURIComponent(csrf)}`,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        method: "POST"
+      })
+      upstreamBody = await upstream.text()
+      console.log("login response", upstream.status, upstream.url)
+    }
+  }
+
+  if (upstream.url.includes("signin_verification_sent")) {
+    await extractToken(upstreamBody)
+    res.status(401).send("Verification sent")
+  }
+  else if (upstream.url.includes("download_members")) {
+    res.send(upstreamBody)
+  }
+  else {
+    console.error("Unexpected response", upstream.status, upstream.url)
+    res.status(400).end()
+  }
+
+  const cookieString = await fetchCookie.cookieJar.getCookieString("https://membermojo.co.uk")
+  await cookieDoc.set({ value: cookieString })
 }
