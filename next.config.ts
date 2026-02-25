b07cfcd3ca3b1f085b79c2077532bf13482142cc
tim.rogers <tim.rogers@justeattakeaway.com>
2026-02-25 14:27:15 +0000

    Rollback any error culprits

--- next.config.ts
+++ next.config.ts
@@ -7,28 +7,6 @@ const nextConfig: NextConfig = {
 			dynamic: 30,
 		},
 	},
-	async headers() {
-		return [
-			{
-				source: "/(.*)",
-				headers: [
-					{
-						key: "Cache-Control",
-						value: "public, s-maxage=3600, stale-while-revalidate=86400",
-					},
-				],
-			},
-			{
-				source: "/api/calendar/feed",
-				headers: [
-					{
-						key: "Cache-Control",
-						value: "public, s-maxage=3600, stale-while-revalidate=86400",
-					},
-				],
-			},
-		]
-	},
 	webpack(config, options) {
 		return config
 	},
