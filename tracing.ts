a53f1db268becfe98e6dc83b2091593d7f38de87
tim.rogers <tim.rogers@justeattakeaway.com>
2026-03-17 16:00:35 +0000

    Export logs

--- tracing.ts
+++ tracing.ts
@@ -1,5 +1,7 @@
 import { NodeSDK } from '@opentelemetry/sdk-node'
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
+import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http'
+import { BatchLogRecordProcessor } from '@opentelemetry/sdk-logs'
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
 import { getAppSecrets } from './lib/secrets'
 
@@ -7,20 +9,24 @@ const honeycombApiKey = getAppSecrets().honeycomb.apiKey
 const eqIndex = honeycombApiKey.indexOf('=')
 const headerKey = honeycombApiKey.slice(0, eqIndex)
 const headerValue = honeycombApiKey.slice(eqIndex + 1)
+const headers = { [headerKey]: headerValue }
+
 const sdk = new NodeSDK({
     traceExporter: new OTLPTraceExporter({
-        headers: {
-            [headerKey]: headerValue
-        },
+        headers,
     }),
+    logRecordProcessors: [
+        new BatchLogRecordProcessor(
+            new OTLPLogExporter({
+                headers,
+            })
+        ),
+    ],
     instrumentations: [
         getNodeAutoInstrumentations({
             '@opentelemetry/instrumentation-fs': {
                 enabled: false,
-            },
-            '@opentelemetry/instrumentation-winston': {
-                enabled: false,
-            },
+            }
         }),
     ],
 })
